0f9214c94068a68957efe99049fbf8ca2bc8762c
Ogidi Franklin <franklinogidi4@gmail.com>
2022-09-21 15:22:12 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - function that return the lenght of a string
- * @s: the string
- * Return: length of s
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*(s + i))
-		i++;
-
-	return (i);
-}
-
-/**
  * _strncat - function that concatenates two strings
  * @dest: first string
  * @src: second string
